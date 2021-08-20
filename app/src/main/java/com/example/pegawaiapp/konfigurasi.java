01af828f079d98fd3fd06f386a28f944c022640b
aladino24 <aladino24@github.com>
2021-08-20 14:25:32 +0700

    Aplikasi Backend

---
+++ app/src/main/java/com/example/pegawaiapp/konfigurasi.java
@@ -0,0 +1,14 @@
+package com.example.pegawaiapp;
+
+public class konfigurasi {
+    public static String BASE_URL="http://192.168.1.103:8080/pegawaiApp/";
+    public static final String URL_GET_ALL=BASE_URL+"/tampilsemuapgw.php";
+    public static final String URL_ADD=BASE_URL+"/tampilpgw.php";
+
+    //JSON TAG
+
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID="id";
+    public static final String TAG_NAME="name";
+
+}
